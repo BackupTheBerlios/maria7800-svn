ea1864fa0882dd293c39b123b6c10c5e67101c77
tom42 <tom42@afd8e475-3d1a-0410-a5d7-ac78ac0245bb>
2006-08-28 19:33:17 +0000

    Removed CartType enum (aloready done.)

--- maria/trunk/emu7800.net/src/Carts.cs
+++ maria/trunk/emu7800.net/src/Carts.cs
@@ -12,38 +12,6 @@ using System.IO;
 
 namespace EMU7800 
 {
-	public enum CartType 
-	{
-		Default,
-		A2K,	// Atari 2kb cart
-		TV8K,	// Tigervision 8kb bankswitched cart
-		A4K,	// Atari 4kb cart
-		PB8K,	// Parker Brothers 8kb bankswitched cart
-		MN16K,	// M-Network 16kb bankswitched cart
-		A16K,	// Atari 16kb bankswitched cart
-		A16KR,	// Atari 16kb bankswitched cart w/128 bytes RAM
-		A8K,	// Atari 8KB bankswitched cart
-		A8KR,	// Atari 8KB bankswitched cart w/128 bytes RAM
-		A32K,	// Atari 32KB bankswitched cart
-		A32KR,	// Atari 32KB bankswitched cart w/128 bytes RAM
-		CBS12K,	// CBS' RAM Plus bankswitched cart w/256 bytes RAM
-		DC8K,	// Special Activision cart (Robot Tank and Decathlon)
-		DPC,	// Pitfall II DPC cart
-		A7808,	// Atari7800 non-bankswitched 8KB cart
-		A7816,	// Atari7800 non-bankswitched 16KB cart
-		A7832,	// Atari7800 non-bankswitched 32KB cart
-		A7832P,	// Atari7800 non-bankswitched 32KB cart w/Pokey
-		A7848,	// Atari7800 non-bankswitched 48KB cart
-		A78SG,	// Atari7800 SuperGame cart
-		A78SGP,	// Atari7800 SuperGame cart w/Pokey
-		A78SGR,
-		A78S9,
-		A78S4,
-		A78S4R,
-		A78AB,	// F18 Hornet cart (Absolute)
-		A78AC,  // Double dragon cart (Activision)
-	};
-
 	[Serializable]
 	public abstract class Cart 
 	{
