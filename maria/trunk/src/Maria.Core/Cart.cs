f78c66e33cb426f51d7e0f231bb5b608192a73f0
tom42 <tom42@afd8e475-3d1a-0410-a5d7-ac78ac0245bb>
2006-08-28 20:26:21 +0000

    Started hacking Cart class.

---
+++ maria/trunk/src/Maria.Core/Cart.cs
@@ -0,0 +1,222 @@
+/*
+ * An abstraction of a game cart.  Attributable to Kevin Horton's Bankswitching
+ * document, the Stella source code, and Eckhard Stolberg's 7800 Bankswitching Guide.
+ *
+ * Copyright (c) 2003, 2004 Mike Murphy
+ * Copyright (C) 2006 Thomas Mathys (tom42@users.berlios.de)
+ *
+ * This file is part of Maria.
+ *
+ * Maria is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Maria is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Maria; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+using System;
+using System.IO;
+using Vtg.Util;
+
+namespace Maria.Core {
+	// TODO : implement IDevice here...
+	[Serializable]
+	public abstract class Cart : IDevice {
+		private Machine machine;
+		private byte[] rom;
+
+		public virtual void Reset() {}
+
+		public virtual bool RequestSnooping {
+			get {
+				return false;
+			}
+		}
+
+		public void Map(AddressSpace mem) {
+			ArgumentCheck.NotNull(mem, "mem");
+			machine = mem.Machine;
+		}
+
+		public abstract byte this[ushort addr] {
+			get;
+			set;
+		};
+
+		// TODO : a bit difficult to decide what to return here.
+		// for carts it should probably be their size *in the address space*,
+		// and not their total amount of storage...we'll decide later.
+		public abstract int Size { get; }
+
+		protected Machine Machine {
+			get { return machine; }
+			set { machine = value; }
+		}
+
+		protected internal byte[] ROM {
+			get { return rom; }
+			set { rom = value; }
+		}
+	}
+}
+
+	// TODO : port shit below
+/*
+		public static Cart New(GameSettings gs)
+		{
+			BinaryReader rom = new BinaryReader(File.OpenRead(gs.FileInfo.FullName));
+			rom.BaseStream.Seek(gs.Offset, SeekOrigin.Begin);
+
+			// TODO : as far as I can tell this whole CartType guessing business
+			//        should probably be part of the GameSettings class. Well.
+			//        Not really neither, but still better than having it lingering
+			//        around here. What I find especially disturbing is that CartType
+			//        is modified...
+			if (gs.CartType == CartType.Default)
+			{
+				FileInfo fi = new FileInfo(gs.FileInfo.FullName);
+				switch (gs.MachineType)
+				{
+					case MachineType.A2600NTSC:
+					case MachineType.A2600PAL:
+						switch (fi.Length - gs.Offset)
+						{
+							case 2048:  gs.CartType = CartType.A2K;  break;
+							case 4096:  gs.CartType = CartType.A4K;  break;
+							case 8192:  gs.CartType = CartType.A8K;  break;
+							case 16384: gs.CartType = CartType.A16K; break;
+							default:
+								throw new Exception("Unexpected CartType: " + gs.CartType.ToString());
+						}
+						break;
+					case MachineType.A7800NTSC:
+					case MachineType.A7800PAL:
+						switch (fi.Length - gs.Offset)
+						{
+							case 8192:  gs.CartType = CartType.A7808; break;
+							case 16384: gs.CartType = CartType.A7816; break;
+							case 32768: gs.CartType = CartType.A7832; break;
+							case 49152: gs.CartType = CartType.A7848; break;
+							default:
+								throw new Exception("Unexpected CartType: " + gs.CartType.ToString());
+						}
+						break;
+				}
+			}
+
+			return Cart.New(rom, gs.CartType);
+		}
+
+		static Cart New(BinaryReader rom, CartType cartType)
+		{
+			Cart c;
+
+			switch (cartType)
+			{
+				case CartType.A2K:
+					c = new CartA2K(rom);
+					break;
+				case CartType.A4K:
+					c = new CartA4K(rom);
+					break;
+				case CartType.A8K:
+					c = new CartA8K(rom);
+					break;
+				case CartType.A8KR:
+					c = new CartA8KR(rom);
+					break;
+				case CartType.A16K:
+					c = new CartA16K(rom);
+					break;
+				case CartType.A16KR:
+					c = new CartA16KR(rom);
+					break;
+				case CartType.DC8K:
+					c = new CartDC8K(rom);
+					break;
+				case CartType.PB8K:
+					c = new CartPB8K(rom);
+					break;
+				case CartType.TV8K:
+					c = new CartTV8K(rom);
+					break;
+				case CartType.CBS12K:
+					c = new CartCBS12K(rom);
+					break;
+				case CartType.A32K:
+					c = new CartA32K(rom);
+					break;
+				case CartType.A32KR:
+					c = new CartA32KR(rom);
+					break;
+				case CartType.MN16K:
+					c = new CartMN16K(rom);
+					break;
+				case CartType.DPC:
+					c = new CartDPC(rom);
+					break;
+				case CartType.A7808:
+					c = new Cart7808(rom);
+					break;
+				case CartType.A7816:
+					c = new Cart7816(rom);
+					break;
+				case CartType.A7832P:
+				case CartType.A7832:
+					c = new Cart7832(rom);
+					break;
+				case CartType.A7848:
+					c = new Cart7848(rom);
+					break;
+				case CartType.A78SGP:
+				case CartType.A78SG:
+					c = new Cart78SG(rom, false);
+					break;
+				case CartType.A78SGR:
+					c = new Cart78SG(rom, true);
+					break;
+				case CartType.A78S9:
+					c = new Cart78S9(rom);
+					break;
+				case CartType.A78S4:
+					c = new Cart78S4(rom, false);
+					break;
+				case CartType.A78S4R:
+					c = new Cart78S4(rom, true);
+					break;
+				case CartType.A78AB:
+					c = new Cart78AB(rom);
+					break;
+				case CartType.A78AC:
+					c = new Cart78AC(rom);
+					break;
+				default:
+					throw new Exception("Unexpected CartType: " + cartType.ToString());
+			}
+			return c;
+		}
+
+		protected void LoadRom(BinaryReader br, int minSize)
+		{
+			// TODO : stream is closed here, but opened somewhere else
+			// => ugly. And perhaps it should be closed in a finally clausle
+			if (br == null)
+			{
+				throw new ArgumentNullException("br");
+			}
+			int flen = (int)(br.BaseStream.Length - br.BaseStream.Position);
+			int size = minSize > flen ? minSize : flen;
+			ROM = new byte[size];
+			br.Read(ROM, 0, size);
+			br.Close();
+		}
+	}
+
+*/
