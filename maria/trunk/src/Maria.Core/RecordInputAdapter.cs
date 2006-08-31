4bee11b8b190cccf5e1ff5642e1d0f5743410828
tom42 <tom42@afd8e475-3d1a-0410-a5d7-ac78ac0245bb>
2006-08-31 11:44:19 +0000

    Added RecordInputAdapter

---
+++ maria/trunk/src/Maria.Core/RecordInputAdapter.cs
@@ -0,0 +1,89 @@
+/*
+ * An InputAdapter extension providing input recording support
+ * TODO: Lightgun needs to be included
+ *
+ * Copyright (C) 2003, 2004 Mike Murphy 
+ * Copyright (C) 2006 Thomas Mathys (tom42@users.berlios.de)
+ *
+ * This file is part of Maria.
+ *
+ * Maria is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Maria is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Maria; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Maria.Core {
+	public class RecordInputAdapter : InputAdapter {
+		private BinaryWriter BW;
+
+		public override string ToString() {
+			return "RecordInputAdapter";
+		}
+
+		private void LogRecordingStatusMsg(string msg) {
+			Trace.Write(this);
+			Trace.Write(": ");
+			Trace.WriteLine(msg);
+		}
+
+		protected override void DoCheckPoint(long frameNumber) {
+			if (BW != null && InputChanged) {
+				try {
+					BW.Write(frameNumber);
+					BW.Write(ConsoleSwitches);
+					BW.Write(KeypadState);
+					for (int i = 0; i < 4; i++) {
+						BW.Write(ControllerActions[i]);
+					}
+					for (int i = 0; i < 4; i++) {
+						BW.Write(Ohms[i]);
+					}
+					for (int i = 0; i < 2; i++) {
+						BW.Write(DrivingCounters[i]);
+					}
+				}
+				catch (Exception ex) {
+					LogRecordingStatusMsg(ex.Message);
+					StopRecording();
+				}
+			}
+		}
+
+		public void StopRecording() {
+			if (BW != null) {
+				BW.Flush();
+				BW.Close();
+				BW = null;
+				LogRecordingStatusMsg("Stopped");
+			}
+		}
+
+		public RecordInputAdapter(string fn, string cartMD5) {
+			try {
+				BW = new BinaryWriter(File.Create(fn));
+				BW.Write("1");
+				BW.Write(cartMD5);
+				BW.Write("EMU7800");
+				BW.Flush();
+				LogRecordingStatusMsg("Started");
+			}
+			catch (Exception ex) {
+				LogRecordingStatusMsg(ex.Message);
+				StopRecording();
+			}
+		}
+	}
+}
