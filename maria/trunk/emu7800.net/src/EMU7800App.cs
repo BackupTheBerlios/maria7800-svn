cfb9c7014f12e1ffe1f1ab5b1699755bce287344
tom42 <tom42@afd8e475-3d1a-0410-a5d7-ac78ac0245bb>
2006-09-02 11:15:44 +0000

    * Moved come code from EMU7800App over to our Maria.cs * Added more detailed info to Maria's ASsemblyInfo.cs * Up'd TODO list

--- maria/trunk/emu7800.net/src/EMU7800App.cs
+++ maria/trunk/emu7800.net/src/EMU7800App.cs
@@ -1,10 +1,10 @@
 /*
  * EMU7800App.cs
- * 
+ *
  * Main application class for EMU7800.
- * 
+ *
  * Copyright (c) 2004-2005 Mike Murphy
- * 
+ *
  */
 using System;
 using System.Diagnostics;
@@ -14,63 +14,10 @@ using System.Runtime.CompilerServices;
 using System.Security.Permissions;
 using System.Windows.Forms;
 
-[assembly: AssemblyTitle("EMU7800")]
-[assembly: AssemblyProduct("EMU7800")]
-[assembly: AssemblyDescription("An Atari 2600/7800 .NET-based Emulator")]
-[assembly: AssemblyCopyright("Copyright © 2003-2006 Mike Murphy")]
-[assembly: AssemblyVersion("0.71.0.0")]
-[assembly: CLSCompliant(false)]
-
 namespace EMU7800
 {
 	public class EMU7800App
 	{
-		public static string Title
-		{
-			get
-			{
-				Assembly myAss = Assembly.GetExecutingAssembly();
-				object[] obj = myAss.GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
-				AssemblyTitleAttribute attr = (AssemblyTitleAttribute)obj[0];
-				return attr.Title;
-			}
-		}
-
-		public static string Version
-		{
-			get
-			{
-				Assembly myAss = Assembly.GetExecutingAssembly();
-				return myAss.GetName().Version.ToString();
-			}
-		}
-
-		public static string Copyright
-		{
-			get
-			{
-				Assembly myAss = Assembly.GetExecutingAssembly();
-				object[] obj = myAss.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
-				AssemblyCopyrightAttribute attr = (AssemblyCopyrightAttribute)obj[0];
-				return attr.Copyright;
-			}
-		}
-
-		public Version ClrVersion
-		{
-			get
-			{
-				return Environment.Version;
-			}
-		}
-		public OperatingSystem OSVersion
-		{
-			get
-			{
-				return Environment.OSVersion;
-			}
-		}
-
 		GlobalSettings _Settings;
 		public GlobalSettings Settings
 		{
@@ -123,20 +70,6 @@ namespace EMU7800
 		private EMU7800App()
 		{
 			EMUTraceListener.Instance.Start();
-
-			Trace.Write(Title);
-			Trace.Write(" v");
-			Trace.Write(Version);
-			Debug.Write(" DEBUG");
-			Trace.WriteLine("");
-			Trace.WriteLine(Copyright);
-
-			Trace.Write("CLR Version: ");
-			Trace.WriteLine(ClrVersion);
-
-			Trace.Write("OS Version: ");
-			Trace.WriteLine(OSVersion);
-
 			_Settings = new GlobalSettings();
 			_ROMProperties = new ROMProperties();
 		}
@@ -155,7 +88,7 @@ namespace EMU7800
 				GameSettings gs = ROMProperties.GetGameSettings(MD5.ComputeMD5Digest(fi));
 				gs.FileInfo = fi;
 				RunMachine(gs, null);
-			} 
+			}
 			catch (Exception ex)
 			{
 				Trace.Write("Error running machine: ");
--- maria/trunk/src/Maria/Maria.cs
+++ maria/trunk/src/Maria/Maria.cs
@@ -19,19 +19,54 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 using System;
-using System.Diagnostics;
+using System.Reflection;
 
 namespace Maria {
 	public class Maria {
+
+		public static string Title {
+			get {
+				Assembly myAss = Assembly.GetExecutingAssembly();
+				object[] obj = myAss.GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+				AssemblyTitleAttribute attr = (AssemblyTitleAttribute)obj[0];
+				return attr.Title;
+			}
+		}
+
+		public static string Version {
+			get {
+				Assembly myAss = Assembly.GetExecutingAssembly();
+				return myAss.GetName().Version.ToString();
+			}
+		}
+
+		public static string Copyright {
+			get {
+				Assembly myAss = Assembly.GetExecutingAssembly();
+				object[] obj = myAss.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+				AssemblyCopyrightAttribute attr = (AssemblyCopyrightAttribute)obj[0];
+				return attr.Copyright;
+			}
+		}
+
+		public static Version ClrVersion {
+			get { return Environment.Version; }
+		}
+
+		public static OperatingSystem OSVersion {
+			get { return Environment.OSVersion; }
+		}
+
 		public static int Main(string[] args) {
 			try {
-				Trace.AutoFlush = true;
-				EmuTraceListener etl = new EmuTraceListener(Console.Out);
-				Trace.Listeners.Add(etl);
-				Trace.WriteLine("Hello, this is a trace message.");
-				Trace.WriteLine("Hello, this is a trace message.");
-				Trace.WriteLine("Hello, this is a trace message.");
-				etl.WriteLine("Passed directly to EmuTraceListener.");
+				// TODO : only the --version switch should output the nonsense below
+				Console.WriteLine("{0} {1}", Title, Version);
+				Console.WriteLine(Copyright);
+				Console.WriteLine("CLR Version : {0}", ClrVersion);
+				Console.WriteLine("OS Version : {0}", OSVersion);
+				// TODO : parse command line
+				// TODO : get global settings from somewhere (system-wide/user-specific)
+				// TODO : get rom properties from somewhere (system-wide/user-specific)
 				return 0;
 			}
 			catch (Exception e) {
@@ -41,4 +76,3 @@ namespace Maria {
 		}
 	}
 }
-
