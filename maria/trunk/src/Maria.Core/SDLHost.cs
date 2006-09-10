831bc698cff402d5719d27e7bb9419e2d6139163
tom42 <tom42@afd8e475-3d1a-0410-a5d7-ac78ac0245bb>
2006-09-10 17:55:46 +0000

    Added some more SDL code...

--- maria/trunk/src/Maria.Core/SDLHost.cs
+++ maria/trunk/src/Maria.Core/SDLHost.cs
@@ -21,6 +21,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Threading;
 using Tao.Sdl;
 
@@ -156,17 +158,9 @@ namespace Maria.Core {
 			}*/
 
 			int startOfFrameTick, endOfRunMachineTick, endOfRenderFrameTick;
-
-		}
-		// TODO : continue implementing the nonsense below...
-/*
-		void Run() {
-			while (!Quit && !M.MachineHalt)
-			{
-				startOfFrameTick = SdlNativeMethods.GetTicks();
-
-				if (M.FrameNumber % FrameSamples == 0)
-				{
+			while (!Quit && !machine.MachineHalt) {
+				startOfFrameTick = Sdl.SDL_GetTicks();
+				if (machine.FrameNumber % FrameSamples == 0) {
 					FPS = 1000.0 * FrameSamples / (RunMachineTicks + RenderFrameTicks + OtherTicks);
 					RunMachineTPF = RunMachineTicks;
 					RunMachineTPF /= FrameSamples;
@@ -179,39 +173,101 @@ namespace Maria.Core {
 					SleepDurationTicks = 0;
 					OtherTicks = 0;
 				}
-
-				SdlNativeMethods.PollAndDelegate();
-
-				if (!Paused)
-				{
-					M.Run();
+				PollEvents();
+				if (!Paused) {
+					machine.Run();
 				}
-
-				endOfRunMachineTick = SdlNativeMethods.GetTicks();
-
+				endOfRunMachineTick = Sdl.SDL_GetTicks();
 				RenderFrame();
+				endOfRenderFrameTick = Sdl.SDL_GetTicks();
 
-				endOfRenderFrameTick = SdlNativeMethods.GetTicks();
-
-				while (WinmmNativeMethods.Enqueue(AudioBuffer) > 0)
-				{
+				// TODO : find out what to do about this...
+				/*
+				while (WinmmNativeMethods.Enqueue(AudioBuffer) > 0) {
 					int duration = SdlNativeMethods.GetTicks() - startOfFrameTick;
-					if (duration > EMU7800App.Instance.Settings.CpuSpin)
-					{
+					if (duration > EMU7800App.Instance.Settings.CpuSpin) {
 						System.Threading.Thread.Sleep(duration);
 						SleepDurationTicks += duration;
 					}
 				}
+				*/
 
 				RunMachineTicks -= startOfFrameTick;
 				RunMachineTicks += endOfRunMachineTick;
 				RenderFrameTicks -= endOfRunMachineTick;
 				RenderFrameTicks += endOfRenderFrameTick;
 				OtherTicks -= endOfRenderFrameTick;
-				OtherTicks += SdlNativeMethods.GetTicks();
+				OtherTicks += Sdl.SDL_GetTicks();
+			}
+		}
+
+		private void PollEvents() {
+			Sdl.SDL_Event evt;
+			while (0 != Sdl.SDL_PollEvent(out evt)) {
+				switch (evt.type) {
+					case Sdl.SDL_KEYDOWN:
+					case Sdl.SDL_KEYUP:
+						Sdl.SDL_KeyboardEvent kev = evt.key;
+						OnKeyboard(
+							kev.which,
+							kev.state == Sdl.SDL_PRESSED,
+							kev.keysym.scancode,
+							kev.keysym.sym,
+							kev.keysym.mod
+						);
+						break;
+					case Sdl.SDL_QUIT:
+						OnQuit();
+						break;
+					default:
+						break;
+				}
+			}
+		}
+		// TODO : port the shit below (from SdlNativeMethods)
+		// into PollEvents() method. Or so. Oh well.
+/*
+				switch ((Event)ev.type)
+				{
+					case Event.JOYBUTTONDOWN:
+					case Event.JOYBUTTONUP:
+						if (JoyButton != null)
+						{
+							SDL_JoyButtonEvent* jev = (SDL_JoyButtonEvent*)&ev;
+							JoyButton(jev->which, jev->button, ev.type == (int)Event.JOYBUTTONDOWN);
+						}
+						break;
+					case Event.JOYAXISMOTION:
+						if (JoyAxis != null)
+						{
+							SDL_JoyAxisEvent* jev = (SDL_JoyAxisEvent*)&ev;
+							JoyAxis(jev->which, jev->axis, jev->val);
+						}
+						break;
+					case Event.MOUSEBUTTONDOWN:
+					case Event.MOUSEBUTTONUP:
+						if (MouseButton != null)
+						{
+							SDL_MouseButtonEvent* mbev = (SDL_MouseButtonEvent*)&ev;
+							MouseButton(mbev->which,
+								(int)mbev->state == (int)State.Pressed,
+								(SdlMouseButton)mbev->button,
+								mbev->x, mbev->y);
+						}
+						break;
+					case Event.MOUSEMOTION:
+						if (MouseMotion != null)
+						{
+							SDL_MouseMotionEvent* mmev = (SDL_MouseMotionEvent*)&ev;
+							MouseMotion((int)mmev->state == (int)State.Pressed,
+								mmev->x, mmev->y, mmev->xrel, mmev->yrel);
+						}
+						break;
+				}
 			}
 		}
 */
+
 		private void SetKeyboardToPlayerNo(int playerno) {
 			ClearPlayerInput(KeyboardPlayerNo);
 			KeyboardPlayerNo = playerno;
@@ -245,6 +301,252 @@ namespace Maria.Core {
 			ia[playerno, ControllerAction.KeypadA] = false;
 			ia[playerno, ControllerAction.Keypad0] = false;
 			ia[playerno, ControllerAction.KeypadP] = false;
+		}
+
+        void SwapPaddles(int deviceno) {
+			// TODO : I'll worry about this later
+			/*
+			if (SdlNativeMethods.Joysticks[deviceno].Opened) {
+				SdlNativeMethods.Joysticks[deviceno].PaddlesSwapper ^= 1;
+				TextMsg = String.Format(
+					"P{0} Stelladptr Paddles {1}wapped",
+					deviceno + 1,
+					SdlNativeMethods.Joysticks[deviceno].PaddlesSwapper == 0 ? "Uns" : "S"
+				);
+			}
+			*/
+        }
+
+		void OnQuit() {
+			Quit = true;
+		}
+
+        void UnPause() {
+            TextMsg = "Resumed";
+            Paused = false;
+        }
+
+		void OnKeyboard(int deviceno, bool down, int scancode, int key, int mod) {
+			InputAdapter ia = machine.InputAdapter;
+            if (Paused) {
+                switch (key) {
+                    case Sdl.SDLK_LCTRL:
+                    case Sdl.SDLK_RCTRL:
+                    case Sdl.SDLK_LALT:
+                    case Sdl.SDLK_RALT:
+                    case Sdl.SDLK_LEFT:
+                    case Sdl.SDLK_UP:
+                    case Sdl.SDLK_RIGHT:
+                    case Sdl.SDLK_DOWN:
+                    case Sdl.SDLK_KP0:
+                    case Sdl.SDLK_KP1:
+                    case Sdl.SDLK_KP2:
+                    case Sdl.SDLK_KP3:
+                    case Sdl.SDLK_KP4:
+                    case Sdl.SDLK_KP5:
+                    case Sdl.SDLK_KP6:
+                    case Sdl.SDLK_KP7:
+                    case Sdl.SDLK_KP8:
+                    case Sdl.SDLK_KP9:
+                    case Sdl.SDLK_KP_MULTIPLY:
+                    case Sdl.SDLK_KP_DIVIDE:
+                        key = Sdl.SDLK_UNKNOWN;
+                        break;
+                }
+            }
+
+			switch (key) {
+				case Sdl.SDLK_ESCAPE:
+					Quit = true;
+					break;
+				case Sdl.SDLK_LCTRL:
+				case Sdl.SDLK_RCTRL:
+					ia[KeyboardPlayerNo, ControllerAction.Trigger] = down;
+					break;
+				case Sdl.SDLK_LALT:
+				case Sdl.SDLK_RALT:
+					ia[KeyboardPlayerNo, ControllerAction.Trigger2] = down;
+					break;
+				case Sdl.SDLK_LEFT:
+					ia[KeyboardPlayerNo, ControllerAction.Left] = down;
+					break;
+				case Sdl.SDLK_UP:
+					ia[KeyboardPlayerNo, ControllerAction.Up] = down;
+					break;
+				case Sdl.SDLK_RIGHT:
+					ia[KeyboardPlayerNo, ControllerAction.Right] = down;
+					break;
+				case Sdl.SDLK_DOWN:
+					ia[KeyboardPlayerNo, ControllerAction.Down] = down;
+					break;
+				case Sdl.SDLK_KP7:
+					ia[KeyboardPlayerNo, ControllerAction.Keypad7] = down;
+					break;
+				case Sdl.SDLK_KP8:
+					ia[KeyboardPlayerNo, ControllerAction.Keypad8] = down;
+					break;
+				case Sdl.SDLK_KP9:
+					ia[KeyboardPlayerNo, ControllerAction.Keypad9] = down;
+					break;
+				case Sdl.SDLK_KP4:
+					ia[KeyboardPlayerNo, ControllerAction.Keypad4] = down;
+					break;
+				case Sdl.SDLK_KP5:
+					ia[KeyboardPlayerNo, ControllerAction.Keypad5] = down;
+					break;
+				case Sdl.SDLK_KP6:
+					ia[KeyboardPlayerNo, ControllerAction.Keypad6] = down;
+					break;
+				case Sdl.SDLK_KP1:
+					ia[KeyboardPlayerNo, ControllerAction.Keypad1] = down;
+					break;
+				case Sdl.SDLK_KP2:
+					ia[KeyboardPlayerNo, ControllerAction.Keypad2] = down;
+					break;
+				case Sdl.SDLK_KP3:
+					ia[KeyboardPlayerNo, ControllerAction.Keypad3] = down;
+					break;
+				case Sdl.SDLK_KP_MULTIPLY:
+					ia[KeyboardPlayerNo, ControllerAction.KeypadA] = down;
+					break;
+				case Sdl.SDLK_KP0:
+					ia[KeyboardPlayerNo, ControllerAction.Keypad0] = down;
+					break;
+				case Sdl.SDLK_KP_DIVIDE:
+					ia[KeyboardPlayerNo, ControllerAction.KeypadP] = down;
+					break;
+				case Sdl.SDLK_1:
+					if (down) {
+						ia[ConsoleSwitch.LDifficultyA] = !ia[ConsoleSwitch.LDifficultyA];
+						TextMsg = "Left Difficulty: "  + (ia[ConsoleSwitch.LDifficultyA] ? "A (Pro)" : "B (Novice)");
+					}
+					break;
+				case Sdl.SDLK_2:
+					if (down) {
+						ia[ConsoleSwitch.RDifficultyA] = !ia[ConsoleSwitch.RDifficultyA];
+						TextMsg = "Right Difficulty: " + (ia[ConsoleSwitch.RDifficultyA] ? "A (Pro)" : "B (Novice)");
+					}
+					break;
+				case Sdl.SDLK_p:
+					if (down && !Paused) {
+						Paused = true;
+						TextMsg = "Paused";
+                        for (int i = 0; i < AudioBuffer.Length; i++) {
+                            AudioBuffer[i] = 0;
+                        }
+					}
+					break;
+				case Sdl.SDLK_F1:
+					if (down) {
+						SetKeyboardToPlayerNo(0);
+					}
+					break;
+				case Sdl.SDLK_F2:
+					if (down) {
+						SetKeyboardToPlayerNo(1);
+					}
+					break;
+				case Sdl.SDLK_F3:
+					if (down) {
+						SetKeyboardToPlayerNo(2);
+					}
+					break;
+				case Sdl.SDLK_F4:
+					if (down) {
+						SetKeyboardToPlayerNo(3);
+					}
+					break;
+				case Sdl.SDLK_F5:
+					PanX = down ? -1 : 0;
+					break;
+				case Sdl.SDLK_F6:
+					PanX = down ? 1 : 0;
+					break;
+				case Sdl.SDLK_F7:
+					PanY = down ? -1 : 0;
+					break;
+				case Sdl.SDLK_F8:
+					PanY = down ? 1 : 0;
+					break;
+				case Sdl.SDLK_F11:
+					if (down) {
+						string fn = String.Format("machinestate{0:yyyy}.{0:MM}-{0:dd}.{0:HH}{0:mm}{0:ss}.emu", DateTime.Now);
+						try {
+							machine.Serialize(Path.Combine(EMU7800App.Instance.Settings.OutputDirectory, fn));
+							Trace.WriteLine("machine state saved successfully.");
+							TextMsg = "Machine State Saved";
+						}
+						catch (Exception e)
+						{
+							Trace.Write("machine state save error: ");
+							Trace.WriteLine(e);
+							TextMsg = "Error Saving Machine State";
+						}
+					}
+					break;
+				case Sdl.SDLK_F12:
+					if (down) {
+						// TODO : I'll worry about screenshots later...
+						/*string fn = String.Format(
+							"screenshot {0:yyyy} {0:MM}-{0:dd} {0:HH}{0:mm}{0:ss}.bmp",
+							DateTime.Now
+						);
+						string rmsg = SdlNativeMethods.SaveBMP(
+							Path.Combine(EMU7800App.Instance.Settings.OutputDirectory, fn)
+						);
+						Trace.WriteLine(rmsg);
+						TextMsg = "Screenshot taken";*/
+					}
+					break;
+				case Sdl.SDLK_c:
+					if (down) {
+						ia[ConsoleSwitch.GameBW] = !ia[ConsoleSwitch.GameBW];
+						TextMsg = ia[ConsoleSwitch.GameBW] ? "B/W" : "Color";
+					}
+					break;
+				case Sdl.SDLK_f:
+					if (down) {
+						TextMsg = String.Format("{0}/{1} FPS {2:0.0} {3:0.0} {4:0.0} {5:0.0}", Math.Round(FPS, 0), EffectiveFPS, RunMachineTPF, RenderFrameTPF, SleepDurationTPF, 1000.0 / EffectiveFPS);
+					}
+					break;
+				case Sdl.SDLK_m:
+					if (down) {
+						Mute = !Mute;
+						TextMsg = Mute ? "Mute On" : "Mute Off";
+					}
+					break;
+				case Sdl.SDLK_r:
+					ia[ConsoleSwitch.GameReset] = down;
+					break;
+				case Sdl.SDLK_s:
+					ia[ConsoleSwitch.GameSelect] = down;
+					break;
+				case Sdl.SDLK_q:
+					if (down) {
+						SwapPaddles(0);
+					}
+					break;
+				case Sdl.SDLK_w:
+					if (down) {
+						SdlJoystickSwapper ^= 1;
+						TextMsg = String.Format("P1/P2 Game Controllrs {0}wapped", SdlJoystickSwapper == 0 ? "Uns" : "S");
+					}
+					break;
+				case Sdl.SDLK_e:
+					if (down) {
+						SwapPaddles(1);
+					}
+					break;
+				default:
+					if (down && Paused) {
+						UnPause();
+					}
+					break;
+			}
+		}
+
+		private unsafe void RenderFrame() {
+			// TODO : do something here...
 		}
 	}
 }
@@ -354,11 +656,6 @@ namespace Maria.Core {
 			SdlNativeMethods.Flip();
 		}
 
-		void OnQuit()
-		{
-			Quit = true;
-		}
-
 		void OnJoyButton(int deviceno, int button, bool down)
 		{
 			InputAdapter ia = M.InputAdapter;
@@ -421,242 +718,6 @@ namespace Maria.Core {
 			}
 		}
 
-		void OnKeyboard(int deviceno, bool down, int scancode, SdlKey key, SdlMod mod)
-		{
-			InputAdapter ia = M.InputAdapter;
-
-            if (Paused)
-            {
-                switch (key)
-                {
-                    case SdlKey.K_LCTRL:
-                    case SdlKey.K_RCTRL:
-                    case SdlKey.K_LALT:
-                    case SdlKey.K_RALT:
-                    case SdlKey.K_LEFT:
-                    case SdlKey.K_UP:
-                    case SdlKey.K_RIGHT:
-                    case SdlKey.K_DOWN:
-                    case SdlKey.K_KP0:
-                    case SdlKey.K_KP1:
-                    case SdlKey.K_KP2:
-                    case SdlKey.K_KP3:
-                    case SdlKey.K_KP4:
-                    case SdlKey.K_KP5:
-                    case SdlKey.K_KP6:
-                    case SdlKey.K_KP7:
-                    case SdlKey.K_KP8:
-                    case SdlKey.K_KP9:
-                    case SdlKey.K_KP_MULTIPLY:
-                    case SdlKey.K_KP_DIVIDE:
-                        key = SdlKey.K_UNKNOWN;
-                        break;
-                }
-            }
-
-			switch (key)
-			{
-				case SdlKey.K_ESCAPE:
-					Quit = true;
-					break;
-				case SdlKey.K_LCTRL:
-				case SdlKey.K_RCTRL:
-					ia[KeyboardPlayerNo, ControllerAction.Trigger] = down;
-					break;
-				case SdlKey.K_LALT:
-				case SdlKey.K_RALT:
-					ia[KeyboardPlayerNo, ControllerAction.Trigger2] = down;
-					break;
-				case SdlKey.K_LEFT:
-					ia[KeyboardPlayerNo, ControllerAction.Left] = down;
-					break;
-				case SdlKey.K_UP:
-					ia[KeyboardPlayerNo, ControllerAction.Up] = down;
-					break;
-				case SdlKey.K_RIGHT:
-					ia[KeyboardPlayerNo, ControllerAction.Right] = down;
-					break;
-				case SdlKey.K_DOWN:
-					ia[KeyboardPlayerNo, ControllerAction.Down] = down;
-					break;
-				case SdlKey.K_KP7:
-					ia[KeyboardPlayerNo, ControllerAction.Keypad7] = down;
-					break;
-				case SdlKey.K_KP8:
-					ia[KeyboardPlayerNo, ControllerAction.Keypad8] = down;
-					break;
-				case SdlKey.K_KP9:
-					ia[KeyboardPlayerNo, ControllerAction.Keypad9] = down;
-					break;
-				case SdlKey.K_KP4:
-					ia[KeyboardPlayerNo, ControllerAction.Keypad4] = down;
-					break;
-				case SdlKey.K_KP5:
-					ia[KeyboardPlayerNo, ControllerAction.Keypad5] = down;
-					break;
-				case SdlKey.K_KP6:
-					ia[KeyboardPlayerNo, ControllerAction.Keypad6] = down;
-					break;
-				case SdlKey.K_KP1:
-					ia[KeyboardPlayerNo, ControllerAction.Keypad1] = down;
-					break;
-				case SdlKey.K_KP2:
-					ia[KeyboardPlayerNo, ControllerAction.Keypad2] = down;
-					break;
-				case SdlKey.K_KP3:
-					ia[KeyboardPlayerNo, ControllerAction.Keypad3] = down;
-					break;
-				case SdlKey.K_KP_MULTIPLY:
-					ia[KeyboardPlayerNo, ControllerAction.KeypadA] = down;
-					break;
-				case SdlKey.K_KP0:
-					ia[KeyboardPlayerNo, ControllerAction.Keypad0] = down;
-					break;
-				case SdlKey.K_KP_DIVIDE:
-					ia[KeyboardPlayerNo, ControllerAction.KeypadP] = down;
-					break;
-				case SdlKey.K_1:
-					if (down)
-					{
-						ia[ConsoleSwitch.LDifficultyA] = !ia[ConsoleSwitch.LDifficultyA];
-						TextMsg = "Left Difficulty: "  + (ia[ConsoleSwitch.LDifficultyA] ? "A (Pro)" : "B (Novice)");
-					}
-					break;
-				case SdlKey.K_2:
-					if (down)
-					{
-						ia[ConsoleSwitch.RDifficultyA] = !ia[ConsoleSwitch.RDifficultyA];
-						TextMsg = "Right Difficulty: " + (ia[ConsoleSwitch.RDifficultyA] ? "A (Pro)" : "B (Novice)");
-					}
-					break;
-				case SdlKey.K_p:
-					if (down && !Paused)
-					{
-						Paused = true;
-						TextMsg = "Paused";
-                        for (int i = 0; i < AudioBuffer.Length; i++)
-                        {
-                            AudioBuffer[i] = 0;
-                        }
-					}
-					break;
-				case SdlKey.K_F1:
-					if (down)
-					{
-						SetKeyboardToPlayerNo(0);
-					}
-					break;
-				case SdlKey.K_F2:
-					if (down)
-					{
-						SetKeyboardToPlayerNo(1);
-					}
-					break;
-				case SdlKey.K_F3:
-					if (down)
-					{
-						SetKeyboardToPlayerNo(2);
-					}
-					break;
-				case SdlKey.K_F4:
-					if (down)
-					{
-						SetKeyboardToPlayerNo(3);
-					}
-					break;
-				case SdlKey.K_F5:
-					PanX = down ? -1 : 0;
-					break;
-				case SdlKey.K_F6:
-					PanX = down ? 1 : 0;
-					break;
-				case SdlKey.K_F7:
-					PanY = down ? -1 : 0;
-					break;
-				case SdlKey.K_F8:
-					PanY = down ? 1 : 0;
-					break;
-				case SdlKey.K_F11:
-					if (down)
-					{
-						string fn = String.Format("machinestate{0:yyyy}.{0:MM}-{0:dd}.{0:HH}{0:mm}{0:ss}.emu", DateTime.Now);
-						try
-						{
-							M.Serialize(Path.Combine(EMU7800App.Instance.Settings.OutputDirectory, fn));
-							Trace.WriteLine("machine state saved successfully.");
-							TextMsg = "Machine State Saved";
-						}
-						catch (Exception e)
-						{
-							Trace.Write("machine state save error: ");
-							Trace.WriteLine(e);
-							TextMsg = "Error Saving Machine State";
-						}
-					}
-					break;
-				case SdlKey.K_F12:
-					if (down)
-					{
-						string fn = String.Format("screenshot {0:yyyy} {0:MM}-{0:dd} {0:HH}{0:mm}{0:ss}.bmp", DateTime.Now);
-						string rmsg = SdlNativeMethods.SaveBMP(Path.Combine(EMU7800App.Instance.Settings.OutputDirectory, fn));
-						Trace.WriteLine(rmsg);
-						TextMsg = "Screenshot taken";
-					}
-					break;
-				case SdlKey.K_c:
-					if (down)
-					{
-						ia[ConsoleSwitch.GameBW] = !ia[ConsoleSwitch.GameBW];
-						TextMsg = ia[ConsoleSwitch.GameBW] ? "B/W" : "Color";
-					}
-					break;
-				case SdlKey.K_f:
-					if (down)
-					{
-						TextMsg = String.Format("{0}/{1} FPS {2:0.0} {3:0.0} {4:0.0} {5:0.0}", Math.Round(FPS, 0), EffectiveFPS, RunMachineTPF, RenderFrameTPF, SleepDurationTPF, 1000.0 / EffectiveFPS);
-					}
-					break;
-				case SdlKey.K_m:
-					if (down)
-					{
-						Mute = !Mute;
-						TextMsg = Mute ? "Mute On" : "Mute Off";
-					}
-					break;
-				case SdlKey.K_r:
-					ia[ConsoleSwitch.GameReset] = down;
-					break;
-				case SdlKey.K_s:
-					ia[ConsoleSwitch.GameSelect] = down;
-					break;
-				case SdlKey.K_q:
-					if (down)
-					{
-						SwapPaddles(0);
-					}
-					break;
-				case SdlKey.K_w:
-					if (down)
-					{
-						SdlJoystickSwapper ^= 1;
-						TextMsg = String.Format("P1/P2 Game Controllrs {0}wapped", SdlJoystickSwapper == 0 ? "Uns" : "S");
-					}
-					break;
-				case SdlKey.K_e:
-					if (down)
-					{
-						SwapPaddles(1);
-					}
-					break;
-				default:
-					if (down && Paused)
-					{
-						UnPause();
-					}
-					break;
-			}
-		}
-
 		void OnMouseButton(int deviceno, bool down, SdlMouseButton button, int x, int y)
 		{
             if (Paused)
@@ -686,24 +747,6 @@ namespace Maria.Core {
 				SetPaddleOhms(KeyboardPlayerNo, WIDTH, MouseX);
 			}
 		}
-
-        void UnPause()
-        {
-            TextMsg = "Resumed";
-            Paused = false;
-        }
-
-        void SwapPaddles(int deviceno)
-        {
-            if (SdlNativeMethods.Joysticks[deviceno].Opened)
-            {
-                SdlNativeMethods.Joysticks[deviceno].PaddlesSwapper ^= 1;
-                TextMsg = String.Format("P{0} Stelladptr Paddles {1}wapped",
-                    deviceno + 1,
-                    SdlNativeMethods.Joysticks[deviceno].PaddlesSwapper == 0 ? "Uns" : "S");
-            }
-        }
-
 	}
 }
 */
